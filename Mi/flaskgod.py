962331f345025c1dc38cfa658f276f0b8d7204bb
stundypurpose <154121793+stundypurpose@users.noreply.github.com>
2024-01-07 22:04:56 +0500

    Add files via upload

---
+++ Mi/flaskgod.py
@@ -0,0 +1,31 @@
+#!/bin/python
+
+from flask import Flask, render_template, request
+import requests
+import json
+import sqlalchemy
+import dask.dataframe as dd
+
+app  = Flask(__name__)
+
+#def get_api():
+
+@app.route("/")
+#def first():
+#	return "HIRE ME BITCH OR I'LL BANKRUPT DATAVIZZ"
+	
+def index():
+	return render_template("date_index.html")
+	
+@app.route('/', methods=['POST'])
+def getvalue():
+	startdate = request.form['startdate']
+	enddate = request.form['enddate']
+	return render_template('pass.html', s=startdate , e=enddate)
+
+@app.route('/')
+def dask():
+#	ddf=dd.read_sql('test_table1',r"mysql://root:root@localhost:3001/test_db","Lat")
+#print(ddf.tail())
+
+app.run(host="0.0.0.0" , port= 80)
